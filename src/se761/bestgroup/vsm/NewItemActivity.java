9cb9bb27f63253097744ad529248fb3d0d79a05d
Michael Little <bio595@gmail.com>
2013-04-07 15:44:00 +1200

    Added post untested request

--- src/se761/bestgroup/vsm/NewItemActivity.java
+++ src/se761/bestgroup/vsm/NewItemActivity.java
@@ -1,17 +1,17 @@
 package se761.bestgroup.vsm;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -27,28 +27,50 @@ public class NewItemActivity extends Activity {
 		@Override
 		protected Boolean doInBackground(String... params) {
 			
-			
-			HttpClient httpclient = new DefaultHttpClient();
-		    HttpPost httppost = new HttpPost("localhost");
+			//instantiates httpclient to make request
+		    DefaultHttpClient httpclient = new DefaultHttpClient();
 
+		    //url with the post data
+		    HttpPost httpost = new HttpPost("vsm.herokuapp.com/new");
+
+		    JSONObject data = new JSONObject();
+			
+			try {
+				data.put("firstName", params[0]);
+				data.put("lastName", params[1]);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+		    //passes the results to a string builder/entity
+		    StringEntity se = null;
+			try {
+				se = new StringEntity(data.toString());
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+				return false;
+			}
+
+		    //sets the post request as the resulting string
+		    httpost.setEntity(se);
+		    //sets a request header so the page receving the request
+		    //will know what to do with it
+		    httpost.setHeader("Accept", "application/json");
+		    httpost.setHeader("Content-type", "application/json");
+		
+		    
 		    try {
-		        // Add your data
-		        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-		        nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-		        nameValuePairs.add(new BasicNameValuePair("stringdata", "Hi"));
-		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-		        // Execute HTTP Post Request
-		        HttpResponse response = httpclient.execute(httppost);
-
-		    } catch (ClientProtocolException e) {
-		       
-		    } catch (IOException e) {
-		       
-		    }
+				httpclient.execute(httpost);
+			} catch (ClientProtocolException e) {
+				
+				e.printStackTrace();
+				return false;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}			
 			
-			Boolean success = false;
-			return success ;
+			return true;
 		}
 		
 	}
