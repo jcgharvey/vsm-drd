3af28ea12944751fbdf0888f485f97e67d82e63c
Michael Little <bio595@gmail.com>
2013-05-06 12:16:56 +1200

    Removed an import statement

--- src/se761/bestgroup/vsm/LoginActivity.java
+++ src/se761/bestgroup/vsm/LoginActivity.java
@@ -7,7 +7,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
